51f56decc360ff77a17d0dba6c12a2527f26884d
Avishkar15 <avishkarrshinde@gmail.com>
2024-12-17 03:08:18 -0500

    Update main.py

--- main.py
+++ main.py
@@ -109,60 +109,60 @@ def create_or_get_playlist(user_id, playlist_name, playlist_description):
             return playlist
     return sp.user_playlist_create(user_id, playlist_name, public=True, description=playlist_description)
 
+def fetch_liked_tracks(sp, max_tracks=200):
+    limit = 50
+    track_ids = []
+    for offset in range(0, max_tracks, limit):
+        liked_songs = sp.current_user_saved_tracks(limit=limit, offset=offset)
+        track_ids.extend([track["track"]["id"] for track in liked_songs["items"]])
+        if not liked_songs["items"]:
+            break
+    return track_ids
+
+
+def fetch_artists_genres(sp, track_ids, genre):
+    batch_size = 50
+    genre_tracks = []
+    for i in range(0, len(track_ids), batch_size):
+        batch = track_ids[i:i + batch_size]
+        tracks_info = sp.tracks(batch)
+        for track in tracks_info['tracks']:
+            for artist in track['artists']:
+                artist_info = sp.artist(artist['id'])
+                if genre.lower() in [g.lower() for g in artist_info["genres"]]:
+                    genre_tracks.append(track['id'])
+    return genre_tracks
+
 
 @app.route('/generate_playlist/', methods=['POST'])
 def generate_playlist():
     cache_handler = spotipy.cache_handler.FlaskSessionCacheHandler(session)
     auth_manager = spotipy.oauth2.SpotifyOAuth(cache_handler=cache_handler)
-    
-    
     sp = spotipy.Spotify(auth_manager=auth_manager)
 
-    current_user = sp.current_user()
-    user_id = current_user['id']
-    genre =request.form['button_text']
-    # Get user's liked songs
-    limit = 50  # Number of songs per request (maximum: 50)
-    offset = 0
-    track_ids = []
+    user_id = sp.current_user()['id']
+    genre = request.form['button_text']
 
-    while len(track_ids) < 200:
-        liked_songs = sp.current_user_saved_tracks(limit=limit, offset=offset)
-        if not liked_songs["items"]:
-            break  # No more liked songs to retrieve
-        track_ids.extend([track["track"]["id"] for track in liked_songs["items"]])
-        offset += limit
-    
-    # Get genre-specific tracks
-    #genre = "k-pop"
-    genre_tracks = []
-    for track_id in track_ids:
-        track_info = sp.track(track_id)
-        track_artists = track_info["artists"]
-        for artist in track_artists:
-            artist_id = artist["id"]
-            artist_info = sp.artist(artist_id)
-            if genre.lower() in [genre.lower() for genre in artist_info["genres"]]:
-                genre_tracks.append(track_id)
-    # Create a new playlist
-    playlist_name = "Liked Songs - {}".format(genre)
-    playlist_description = "Playlist of liked songs in the {} genre".format(genre)
+    # Fetch liked songs efficiently
+    track_ids = fetch_liked_tracks(sp, max_tracks=200)
+
+    # Retrieve genre-specific tracks in fewer API calls
+    genre_tracks = fetch_artists_genres(sp, track_ids, genre)
+
+    # Create or fetch playlist
+    playlist_name = f"Liked Songs - {genre}"
+    playlist_description = f"Playlist of liked songs in the {genre} genre"
     playlist = create_or_get_playlist(user_id, playlist_name, playlist_description)
 
-    existing_tracks = set()
-    playlist_items = sp.playlist_items(playlist["id"], fields="items(track.id)", limit=100)
-    for item in playlist_items["items"]:
-        existing_tracks.add(item["track"]["id"])
+    # Add tracks to playlist
+    existing_tracks = {item['track']['id'] for item in sp.playlist_items(playlist['id'], fields="items(track.id)")['items']}
+    tracks_to_add = [track for track in genre_tracks if track not in existing_tracks]
 
-    # Add genre-specific tracks to the playlist if they are not already present
-    tracks_to_add = [track_id for track_id in genre_tracks if track_id not in existing_tracks]
+    if tracks_to_add:
+        for i in range(0, len(tracks_to_add), 100):
+            sp.playlist_add_items(playlist['id'], tracks_to_add[i:i + 100])
 
-    batch_size = 100  # Number of tracks per batch request
-    for i in range(0, len(tracks_to_add), batch_size):
-        batch = tracks_to_add[i:i + batch_size]
-        sp.playlist_add_items(playlist["id"], batch)
-
-    return render_template('app/playlist.html',playlist_name=playlist_name)
+    return render_template('app/playlist.html', playlist_name=playlist_name)
 
 @app.route('/similar_songs/', methods=['POST'])
 def similar_songs():
